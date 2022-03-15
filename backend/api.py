8a4d08da3c380c33b0dfb28193815b41110e9855
thomasdrabo <49268700+thomasdrabo@users.noreply.github.com>
2022-03-15 14:46:42 +0100

    add ghunt

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,14 @@
 import flask
 import os
+import subprocess
 from flask_cors import CORS, cross_origin
 from flask import jsonify
+
 app = flask.Flask(__name__)
 cors = CORS(app)
+
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 @cross_origin()
 
 @app.route('/api/domain/<domain>', methods=['GET'])
@@ -19,5 +23,12 @@ def show_domain(domain):
     return jsonify(test)
 
 
+
+@app.route('/api/email/<email>', methods=['GET'])
+def find_by_mail(email):
+        return "toto"  
+    # command = f"cd ../test/ghunt ; py -m ghunt.py {email}" 
+    # ret = subprocess.run(command, capture_output=True, shell=True)
+ 
 
 app.run()
