c0b7d6192e939b047a484136555d538d845186cf
Mathias Lunardon <mathias@mathiaslunardon.com>
2022-03-15 11:38:53 +0100

    go

--- backend/api.py
+++ backend/api.py
@@ -6,9 +6,18 @@ app = flask.Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
+
 @app.route('/api/domain/<domain>', methods=['GET'])
-def show_user_profile(domain):
-    print(domain)
-    return jsonify(domain)
+def show_domain(domain):
+    test = {
+        "domain": domain,
+        "yes": "bien"
+    }
+    print("--------------------------------")
+    print(os.popen('ls').read())
+    print("--------------------------------")
+    return jsonify(test)
+
+
 
 app.run()
