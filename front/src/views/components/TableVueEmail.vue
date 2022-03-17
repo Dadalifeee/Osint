46855df761d5b18f135901a7304f04a25473c7eb
Mathias Lunardon <mathias@mathiaslunardon.com>
2022-03-17 14:59:01 +0100

    update mail

--- front/src/views/components/TableVueEmail.vue
+++ front/src/views/components/TableVueEmail.vue
@@ -1,16 +1,9 @@
 <template lang="">
   <div>
     <div v-if="dataEmail.length > 0">
-      <p>Mail : {{dataEmail[0].Mail}}</p>
-      <p>Account matches : {{dataEmail[1].Account_matches}}</p>
-      <p>Accounts Name : {{dataEmail[2].Accounts_Name}}</p>
-      <p>Last profile edit : {{dataEmail[3].Last_profile_edit}}</p>
-      <p>Email : {{dataEmail[4].Email}}</p>
-      <p>Gaia ID : {{dataEmail[5].Gaia_ID}}</p>
-      <p>Hangouts Bot : {{dataEmail[6].Hangouts_Bot}}</p>
-      <p>YouTube channel : {{dataEmail[7].YouTube_channel}}</p>
-
-      <!-- <img :src="'../../../../backend/email/profile_pics/' + dataEmail[0].Mail"/> -->
+      <div v-for="(item, index) in dataEmail" :key="index">
+        {{Object.keys(item)[0]}} : {{Object.values(item)[0]}}
+      </div>
     </div>
     
   </div>
